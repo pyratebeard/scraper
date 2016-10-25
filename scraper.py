722e4d96297e07633f5bf805ab5138c327ba39ae
dudley <pyratebeard@gmail.com>
2016-10-25 18:01:41 +0100

    removed traceback module - not needed. main function written. improved exceptions"

--- scraper.py
+++ scraper.py
@@ -13,7 +13,6 @@ from getpass import getpass
 from datetime import datetime, timedelta
 import socket
 import pxssh
-import traceback
 
 # set global variables
 userHome = os.path.expanduser("~")
@@ -119,127 +118,52 @@ def main():
 
 	getArgs()
 
-	# get creds
-	user = raw_input('Username: ')
-	passwd = getpass.getpass('Password: ')
+	if args.output is not None:
+		logConf()
 
-	# if no command file, get command
+	writeLog("===== STARTING =====", "INFO")
+
+	user, passwd = checkToken()
+
+	# if no command file, enter single command
 	if not args.command:
-		command = raw_input('Command: ')
+		global command
+		command = raw_input("Command: ")
 
-	global system # for use in exception
-	servers = [line.strip() for system in open(args.input)]
+	servers = [server.strip() for server in open(args.input)]
 
-	# set up counter for terminal output
-	total_num = len(servers)
+	# for stdout if an output file is used
+	global totalNum
+	totalNum = len(servers)
 	num = 0
 
-	for system in servers:
-		s = pxssh.pxssh()
+	for server in servers:
 		try:
-			# check if the system is online
-			socket.getaddrinfo(system,0,0,0,0)
-			try:
-				s.login(system, user, passwd)
-				if not args.output: # if no output file print to terminal
-					print '%s - ssh session login successful' % system
-					if not args.command: # if no command file, use command from input
-						s.sendline(command)
-						s.prompt()
-						print s.before
-						s.logout
-						s.close
-					else:
-						# read the commands in file
-						commands = [line.strip() for line in open(args.command)]
-						for c in range(len(commands)): # issue commands line by line
-							s.sendline(commands[i])
-							s.prompt()
-							print s.before
-							s.logout
-							s.close
-				else:
-					# output the counter
-					num = num +1
-					sys.stdout.write('[%s/%s] %s \r' % \
-										(num, total_num, system))
-					sys.stdout.flush()
-					f = open(args.output, 'a')
-					log = '%s - ssh session login successful\n' % system
-					f.write(log)
-					f.close
-					if not args.command: # if no command file, use command from input
-						s.sendline(command)
-						s.prompt()
-						log = '%s\n' % s.before
-						f.write(log)
-						f.close
-						s.logout
-						s.close
-					else:
-						# read the commands in file
-						commands = [line.strip() for line in open(args.command)]
-						for c in range(len(commands)): # issue commands line by line
-							s.sendline(commands[i])
-							s.prompt()
-							log = '%s\n' % s.before
-							f.write(log)
-							f.close
-							s.logout
-							s.close
-			except Exception, e:
-				if 'password refused' in e:
-					if not args.output:
-						print '%s - password refused' % system
-						print str(s)
-					else:
-						num = num +1
-						sys.stdout.write('[%s/%s] %s \r' % \
-										(num, total_num, system))
-						sys.stdout.flush()
-						f = open(args.output, 'a')
-						log = '%s - password refused\n' % system
-						f.write(log)
-						f.close
-					continue
-				else:
-					if not args.output:
-						print '%s - another issue' % system
-						print str(s)
-					else:
-						num = num +1
-						sys.stdout.write('[%s/%s] %s \r' % \
-										(num, total_num, system))
-						sys.stdout.flush()
-						f = open(args.output, 'a')
-						log = '%s - another issue\n' % system
-						f.write(log)
-						f.close
-					continue
+			num = num + 1
+			# this can be improved to clear the whole line...
+			sys.stdout.write("[%s/%s] %s\t\t\r" %(num, totalNum, server))
+			sys.stdout.flush()
+			# ensure the hostname exists before attempting to log on
+			socket.getaddrinfo(server,0,0,0,0)
+			msg = "Hostname %s did resolve" % server
+			writeLog(msg, "INFO")
+
+			login(server, user, passwd)
+
 		except socket.gaierror:
-			if not args.output:
-				print '%s - hostname did not resolve' % system
-				print str(s)
-			else:
-				num = num +1
-				sys.stdout.write('[%s/%s] %s \r' % \
-								(num, total_num, system))
-				sys.stdout.flush()
-				f = open(args.output, 'a')
-				log = '%s - hostname did not resolve\n' % system
-				f.write(log)
-				f.close
-			continue
+			msg = "Hostname %s did not resolve" % server
+			writeLog(msg, "WARN")
+
+	writeLog("===== FINISHED =====", "INFO")
+	sys.stdout.flush()
+	print("\nFinished\r")
 
 if __name__ == "__main__":
 	try:
 		main()
-	except Exception, e:
-		print str(e)
-		traceback.print_exc()
-		print 'Hostname = %s' % system
+	except (Exception, RuntimeError, NameError, TypeError) as e:
+		print(str(e))
 		os._exit(1)
 	except KeyboardInterrupt:
-		print
-		print 'Exited by user'
+		print("\nExited by user")
 		os._exit(1)
